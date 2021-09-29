0ca7c8c36cb6b0cf52eaa5dab49a044622769270
Rony163 <minhajulislamrony43@gmail.com>
2021-09-29 16:50:47 +0600

    first commit

---
+++ src/components/Culture/Culture.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Culture = () => {
+    return (
+        <div>
+            <h2>This is from culture</h2>
+            <p>We have free wate and food</p>
+        </div>
+    );
+};
+
+export default Culture;
